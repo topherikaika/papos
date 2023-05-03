cb47cab942880ad7823b9e35d6661eab3c0499fe
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-05-03 11:46:31 +0900

    create response for homepage

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
 let db;
+const uri = "mongodb+srv://memorias:memorias123@cluster0.kohzkj0.mongodb.net/papos?retryWrites=true&w=majority";
 const app = express();
 
 app.get("/", async (req, res) => {
   const allRecipes = await db.collection("recipes").find().toArray();
   console.log(allRecipes);
-  console.log("Welcome to the Homepage");
+  res.send("Welcome to the Homepage");
 });
 
 app.get("/admin", (req, res) => {
@@ -14,7 +15,7 @@ app.get("/admin", (req, res) => {
 });
 
 async function start() {
-  const client = new MongoClient();
+  const client = new MongoClient(uri);
   await client.connect();
   db = client.db();
   app.listen(8080);
