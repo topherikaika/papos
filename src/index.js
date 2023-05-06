ea5f7b6d6525f5977e10d2cff0e2244388316710
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-05-06 11:23:56 +0900

    create CreateNewForm elements and import in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import Axios from "axios";
+/* COMPONENTS */
+import CreateNewForm from "./components/CreateNewForm";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
