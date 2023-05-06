b6d985431a17c9aa95c5cb151c97bd00c2cdc957
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-05-06 10:02:13 +0900

    create react body boilerplate

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Hey</h1>
+      <p>This is REACT</p>
+    </div>
+  );
+}
