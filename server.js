e2948abb6ca12b80200fb36d35d2ce6c57e8b1be
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-05-06 12:29:59 +0900

    install sanitize-html and create ourCleanup function to leverage sanitize-html

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const uri = "mongodb+srv://memorias:memorias123@cluster0.kohzkj0.mongodb.net/pap
 const app = express();
 const multer = require("multer");
 const upload = multer();
+const sanitizeHTML = require("sanitize-html")
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -32,6 +33,16 @@ app.post("/create-recipe", upload.single("photo"), async (req, res) => {
   console.log(req.body);
   res.send("Thank you");
 });
+
+function ourCleanup(req, res, next) {
+  if (typeof req.body.name != "string") req.body.name = ""
+  if (typeof req.body.type != "string") req.body.type = ""
+  if (typeof req.body._id != "string") req.body._id = ""
+
+  req.cleanData = {
+    name:
+  }
+}
 
 async function start() {
   const cloud = new MongoClient(uri);
