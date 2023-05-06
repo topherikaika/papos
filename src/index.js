407e394e8417ce451337930295718afa2034b46b
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-05-06 10:14:15 +0900

    create const recipes empty array and map recipes in order to recturn react animal card outputting recipe name and recipe type

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 function App() {
+  const recipes = [];
   return (
     <div>
       <h1>Hey</h1>
       <p>This is REACT</p>
+      {recipes.map(function (recipe) {
+        return <AnimalCard name={recipe.name} type={recipe.type} />;
+      })}
     </div>
   );
 }
 
 function RecipeCard(props) {
-  return (
-    <p>
-      {props.name} e {props.type}
-    </p>
-  );
+  return;
+  <p>
+    {props.name} e {props.type}
+  </p>;
 }
 
 const root = createRoot(document.querySelector("#app"));
