7140ddbaf429af6d4da269b2ffeab5decfde8b3f
Christopher Ikaika Crespo <topherikaika@gmail.com>
2024-01-28 11:37:22 +0900

    correct const app's directory

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const client = new MongoClient(process.env.CONNECTIONSTRING);
 async function start() {
   await client.connect();
   module.exports = client;
-  const app = require("./app");
+  const app = require("./backend-api/app");
   app.listen(process.env.PORT);
 }
 
